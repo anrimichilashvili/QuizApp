406b26e811f4bd36e020f30cec9e6f0dbee9610f
Anri Michilashvili <amichilashvili@rsm.ge>
2024-01-05 16:34:15 +0400

    First Commit

---
+++ database/migrations/2024_01_04_122821_create_questions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('Question');
+            $table->string('answer1');
+            $table->string('answer2');
+            $table->string('answer3');
+            $table->string('answer4');
+            $table->string('correct_answer');
+            $table->string('Photo')->nullable();;
+
+            //$table->unsignedBigInteger('quiz_id');
+            //$table->foreign('quiz_id')->references('id')->on('quizzes');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
